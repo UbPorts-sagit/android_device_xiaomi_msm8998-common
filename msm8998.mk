10e1a9c1d71e2ba34c3e4b199cbd8abd1e5fc95f
Erfan Abdi <erfangplus@gmail.com>
2020-03-30 01:13:25 +0430

    ubport: Build sensorservice

--- msm8998.mk
+++ msm8998.mk
@@ -428,6 +428,9 @@ PRODUCT_PACKAGES += \
     libbiometry_fp_api \
     libubuntu_application_api
 
+PRODUCT_PACKAGES += \
+    sensorservice
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ubports/70-sagit.rules:system/halium/lib/udev/rules.d/70-android.rules \
     $(LOCAL_PATH)/ubports/scaling.conf:system/halium/etc/ubuntu-touch-session.d/android.conf
