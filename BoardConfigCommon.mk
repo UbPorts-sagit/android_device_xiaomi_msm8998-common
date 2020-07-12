2a44885bec5f1fbbc3a0f4413f6e84dd444d7ecd
verevka <verevka86@gmail.com>
2020-07-12 23:23:54 +0300

    cmdline: Disable audit

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,7 +53,7 @@ BOARD_KERNEL_CMDLINE := androidboot.console=ttyMSM0 androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
-BOARD_KERNEL_CMDLINE += apparmor=1 security=apparmor
+BOARD_KERNEL_CMDLINE += apparmor=1 security=apparmor audit=0
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8998
