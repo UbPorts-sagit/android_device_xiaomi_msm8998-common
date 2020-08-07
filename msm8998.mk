c6725f35a9569c78eca246b7c7c82c993d6db41c
verevka <verevka86@gmail.com>
2020-08-07 09:55:11 +0300

    Build charger_res_images

--- msm8998.mk
+++ msm8998.mk
@@ -301,6 +301,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+## Power off charging mode
+PRODUCT_PACKAGES += \
+    charger_res_images
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
