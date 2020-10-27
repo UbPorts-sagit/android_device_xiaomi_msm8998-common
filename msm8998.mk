070c70d65dd5f3ef292a5e214aa641634f1b6eac
verevka <verevka86@gmail.com>
2020-10-27 11:52:47 +0300

    Revert "msm8998-common: Add ThermalController"
    This reverts commit c1db7128f555e97d678f11074bc5091ca5720dc5.

--- msm8998.mk
+++ msm8998.mk
@@ -207,9 +207,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
 
-PRODUCT_PACKAGES += \
-    ThermalController
-
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
